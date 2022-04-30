2dad4183add5640ae1f2adfa6b016c806ae8a2d3
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-04-30 21:33:38 +0800

    Time: 131 ms (63.65%), Space: 11.3 MB (88.34%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.c
+++ 217-contains-duplicate/217-contains-duplicate.c
@@ -1,13 +1,67 @@
-static int _cmp(const int *l, const int *r)  
-{ 
-    return (*l - *r); 
+void swap(int* a, int* b){
+
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
 
-bool containsDuplicate(int* nums, int numsSize) {
-    qsort(nums, numsSize, sizeof(int), _cmp);
+int Partition(int* array,int front, int end){
+
+    int pivotkey;
+    int mid=front+(end-front)/2;
     
-    for (int i = 1; i < numsSize; ++i) {
-        if (nums[i] == nums[i - 1]) return true;
+    if(array[front]>array[end]){
+        swap(&array[front],&array[end]);
+    }
+    if(array[mid]>array[end]){
+        swap(&array[mid],&array[end]);
+    }
+    if(array[mid]>array[front]){
+        swap(&array[mid],&array[front]);
+    }
+    
+    pivotkey=array[front];
+
+    while(front<end){
+
+        while (front<end && array[end]>=pivotkey){
+            end--;
+        }
+        swap(&array[end],&array[front]);
+
+        while (front<end && array[front]<=pivotkey){
+            front++;
+        }
+        swap(&array[end],&array[front]);
+
+    }
+
+    return front;
+
+}
+
+void quicksort(int* array, int front, int end){
+
+    int pivot;
+
+    if(front<end){
+
+        pivot=Partition(array,front,end);
+        quicksort(array,front,pivot-1);
+        quicksort(array,pivot+1,end); 
+    }
+}
+
+
+bool containsDuplicate(int* nums, int numsSize){
+    
+    quicksort(nums,0,numsSize-1);
+    
+    for (int i=0;i<numsSize-1;i++){
+        
+        if (nums[i]==nums[i+1])
+            return true;
+        
     }
     
     return false;
