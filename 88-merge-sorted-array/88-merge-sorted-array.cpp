8d9c2ce0e0c886266d8887181240c18d96a768cb
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-01 15:43:19 +0800

    Time: 7 ms (20.92%), Space: 9.1 MB (31.52%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,16 +1,15 @@
-int cmp ( const void *a , const void *b ) { 
-    return *(int *)a - *(int *)b; 
-}
-
-
-void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+class Solution {
+public:
     
-    for(int i=m-1,j=0;j<n;j++){
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
-        nums1[i+j+1]=nums2[j];
+        for(int i=m-1,j=0;j<n;j++){
+
+            nums1[i+j+1]=nums2[j];
+
+        }
+              
+        sort(nums1.begin(),nums1.end()); 
         
     }
-    
-    qsort(nums1,nums1Size,sizeof(int),cmp); 
-    
-}
+};
