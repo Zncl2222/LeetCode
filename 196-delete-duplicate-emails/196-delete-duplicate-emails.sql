763009c1a4f6d3c19cab4b64d87f070bb9e5effd
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-05 00:00:06 +0800

    Time: 1021 ms (18.10%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,5 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE t1
+FROM Person t1, Person t2
+WHERE t1.email=t2.email AND t1.id>t2.id
