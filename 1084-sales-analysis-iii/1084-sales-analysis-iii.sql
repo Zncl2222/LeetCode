1b7f6c7ecf7de894fa00145e30d0d11eb991a782
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-04 10:08:48 +0800

    Time: 1095 ms (62.72%), Space: 0B (100.00%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT product_id, product_name
+FROM Product
+WHERE product_id NOT IN
+(SELECT product_id FROM Sales WHERE sale_date>"2019-03-31" OR sale_date < "2019-01-01")
