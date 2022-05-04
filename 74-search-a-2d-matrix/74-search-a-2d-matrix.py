819b3463823f9a651904d02f1c51d7421677a5b4
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-04 23:29:56 +0800

    Time: 52 ms (69.31%), Space: 14.3 MB (99.60%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -2,7 +2,6 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
         row_size=len(matrix)
-        col_size=len(matrix[0])
         
         for i in range(row_size):
             
