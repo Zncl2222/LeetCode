2022bd23413cb37582e70233cb4fb78440605bfa
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-06 11:55:45 +0800

    Time: 7 ms (94.66%), Space: 8.1 MB (17.35%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.c
+++ 141-linked-list-cycle/141-linked-list-cycle.c
@@ -7,19 +7,21 @@
  */
 bool hasCycle(struct ListNode *head) {
 
-    if (head==NULL || head->next==NULL){
+    
+    if(head==NULL || head->next==NULL)
         return false;
-    }
     
     struct ListNode* fast=head;
     struct ListNode* slow=head;
     
     while(fast!=NULL && fast->next!=NULL){
-        slow=slow->next;
+        
         fast=fast->next->next;
-        if (slow==fast)
+        slow=slow->next;
+        
+        if(slow==fast)
             return true;
     }
-    
+        
     return false;
 }
