166e1f3a5281565995d61374834ce7662ed08ae1
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-09 11:16:10 +0800

    Time: 0 ms (100.00%), Space: 5.7 MB (74.49%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.c
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.c
@@ -9,27 +9,28 @@
 
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     
-    if(head->next==NULL)
+    if(head==NULL || head->next==NULL)
         return NULL;
     
-    struct ListNode *p=head,*q=head;
+    struct ListNode* ptr1=head;
+    struct ListNode* ptr2=head;
     
-    for (int i=0;i<n;i++){
+    for(int i=0; i<n; i++){
         
-        p=p->next;
+        ptr1=ptr1->next;
     }
-
     
-    if (p==NULL)
+    if(ptr1==NULL)
         return head->next;
     
-    while(p->next!=NULL){
-        p=p->next;
-        q=q->next;
+    while(ptr1->next!=NULL){
+        
+        ptr1=ptr1->next;
+        ptr2=ptr2->next;
+        
     }
-
     
-    q->next=q->next->next;
-
+    ptr2->next=ptr2->next->next;
+     
     return head;
 }
