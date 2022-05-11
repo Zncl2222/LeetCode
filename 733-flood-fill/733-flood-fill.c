3f9198bfb5f06ae02e12e144eb9f45b74b4c342f
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-11 09:33:35 +0800

    Time: 30 ms (43.29%), Space: 8 MB (67.68%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.c
@@ -0,0 +1,30 @@
+
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+void fill_traverse(int** image, int row_size, int col_size, int sr, int sc, int origin_color, int newColor){
+    
+    if(sr<0 || sr>=row_size || sc<0 || sc>=col_size || image[sr][sc]!=origin_color || image[sr][sc]==newColor)
+        return 0;
+    
+    image[sr][sc]=newColor;
+    
+    fill_traverse(image, row_size, col_size, sr+1, sc, origin_color, newColor);
+    fill_traverse(image, row_size, col_size, sr-1, sc, origin_color, newColor);
+    fill_traverse(image, row_size, col_size, sr, sc+1, origin_color, newColor);
+    fill_traverse(image, row_size, col_size, sr, sc-1, origin_color, newColor);     
+}
+
+
+int** floodFill(int** image, int imageSize, int* imageColSize, int sr, int sc, int newColor, int* returnSize, int** returnColumnSizes){
+        
+    *returnSize=imageSize;
+    *returnColumnSizes=imageColSize;
+    
+    fill_traverse(image, imageSize, imageColSize[0], sr, sc, image[sr][sc] ,newColor);
+
+    return image;
+}
