1fa2ab9f198302653dfe1f3582c2359842e0d5e6
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-14 09:13:05 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (71.10%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.c
@@ -9,30 +9,30 @@
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     
-    struct ListNode res;
-    struct ListNode* ptr=&res;
+    struct ListNode* res = malloc(sizeof(struct ListNode));
+    struct ListNode* ptr = res;
     
-    while(list1 && list2){
+    while(list1 != NULL && list2 != NULL){
         
         if (list1->val <= list2->val){
             
-            ptr->next=list1;
-            list1=list1->next;
-            ptr=ptr->next;
+            ptr->next = list1;
+            list1 = list1->next;
+            ptr = ptr->next;
         }
         else{
-            ptr->next=list2;
-            list2=list2->next;
-            ptr=ptr->next;
-            
+            ptr->next = list2;
+            list2 = list2->next;
+            ptr = ptr->next;           
         }
         
     }
     
-    if(list1)
-        ptr->next=list1;
+    if(list2 != NULL)
+        ptr->next = list2;
     else
-        ptr->next=list2;
-    
-    return res.next;
-};
+        ptr->next = list1;
+
+
+    return res->next;
+}
