5dcd459e72e5713c70f944d51b6370656352cd5d
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-19 14:02:26 +0800

    Time: 5 ms (55.29%), Space: 6.4 MB (22.48%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.c
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.c
@@ -1,27 +1,27 @@
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target){
 
-    int matCol_size=matrixColSize[0];
-    int target_row=0;
+     if(matrixSize == 0)
+        return false;
     
-    for (int i=0;i<matrixSize-1;i++){
+    int col_size = matrixColSize[0];
+    int target_row = 0;
+    
+    for(int i = 0; i < matrixSize; i++){
         
-        if(target>=matrix[target_row][0] && target<=matrix[target_row][matCol_size-1])
-            break;
-        else
+        if(matrix[i][col_size-1] < target)
             target_row++;
-            
+        else
+            break;       
     }
-
-    if (target_row>matrixSize)
-        return false; 
     
-    for(int i=0;i<matCol_size;i++){
-
-        if(matrix[target_row][i]==target)
-            return true;
+    if(target_row > matrixSize-1)
+        return false;
+    
+    for(int i = 0; i < col_size; i++){
         
+        if(target == matrix[target_row][i])
+            return true;  
     }
-       
-    return false;
     
+    return false;
 }
