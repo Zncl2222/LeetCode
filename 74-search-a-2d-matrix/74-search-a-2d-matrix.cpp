4a75f2a4cc181347578225fd2d66a0b3334d46b1
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-20 10:49:50 +0800

    Time: 3 ms (88.51%), Space: 9.6 MB (53.54%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -5,19 +5,17 @@ public:
         int row_size=matrix.size();
         int col_size=matrix[0].size();
         
-        int curr_row=0, curr_col=col_size-1;
+        int curr_row = 0, curr_col = col_size - 1;
         
-        while(curr_row<row_size && curr_col>-1){
+        while(curr_row < row_size && curr_col > -1){
             
-            int curr=matrix[curr_row][curr_col];
-            
-            if (curr==target)
+            if(matrix[curr_row][curr_col] == target)
                 return true;
             
-            if (target>curr)
+            if(target > matrix[curr_row][curr_col])
                 curr_row++;
             else
-                curr_col--;            
+                curr_col--;
             
         }
         
