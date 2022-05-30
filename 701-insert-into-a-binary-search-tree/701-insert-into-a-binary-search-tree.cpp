00eb96265d220195618aac72de0740878f7726a2
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-05-30 10:31:00 +0800

    Time: 142 ms (22.35%), Space: 57 MB (50.27%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.cpp
@@ -14,16 +14,17 @@ public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         
         if(root == NULL){
-            root = new TreeNode(val);
+            
+            root = new(struct TreeNode);
+            root->val = val;
             return root;
         }
-        
-        if(root->val < val)
-            root->right = insertIntoBST(root->right, val);
-        else if(root->val > val)
+            
+        if(root->val > val)
             root->left = insertIntoBST(root->left, val);
+        else if(root->val < val)
+            root->right = insertIntoBST(root->right,val);
         
         return root;
-        
     }
 };
