99b3a654223e52377ed2f629a3bf679d3fded57f
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-07-03 20:05:23 +0800

    Time: 7 ms (84.34%), Space: 7 MB (13.56%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -2,26 +2,16 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-int sum(int* nums, int current ,int numsSize){
-    
-    if(current == 0)
-        return nums[0];
-    
-    return nums[current] + sum(nums, current - 1, numsSize);
-}
-
 int* runningSum(int* nums, int numsSize, int* returnSize){
     
     *returnSize = numsSize;
     int* res = (int*)malloc(numsSize*sizeof(int));
-    int current = 0;
-    
+    int curr_sum = 0;
     for(int i = 0; i < numsSize; i++){
         
-        res[numsSize-1-i] = sum(nums, numsSize-i-1, numsSize);
-        
+        curr_sum += nums[i];
+        res[i] = curr_sum;
     }
     
     return res;
-
 }
