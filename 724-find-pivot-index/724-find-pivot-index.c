83ae194879e17304934fd9e7ac453229c98a0250
Zncl2222 <77830479+Zncl2222@users.noreply.github.com>
2022-07-03 22:02:39 +0800

    Time: 35 ms (46.26%), Space: 6.5 MB (68.14%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.c
+++ 724-find-pivot-index/724-find-pivot-index.c
@@ -5,33 +5,29 @@ int pivotIndex(int* nums, int numsSize){
     int leftsum = 0;
     int rightsum = 0;
     int pivot = 0;
-    int flag = 0;
-    for(int i = 0; i < numsSize; i++){
 
-        if(flag == 0){
-            
-            for(int j = 0; j < pivot; j++){
+    for(int j = 0; j < pivot; j++){
 
-                leftsum += nums[j];
-            }
+        leftsum += nums[j];
+    }
+    for(int j = pivot + 1; j < numsSize; j++){
 
-            for(int j = pivot + 1; j < numsSize; j++){
+        rightsum += nums[j];
+    }
+    
+    if(leftsum == rightsum)
+        return pivot;
+    
+    ++pivot;
 
-                rightsum += nums[j];
-            }
-            ++flag;
-        }
-        else{
-        
-            leftsum += nums[pivot - 1];
-            rightsum -= nums[pivot];
-        }
+    for(int i = pivot; i < numsSize; i++){
+
+        leftsum += nums[pivot - 1];
+        rightsum -= nums[pivot];
 
         if(leftsum == rightsum)
             return pivot;
-        
         ++pivot;
-        
     }
     
     return -1;
