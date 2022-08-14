896caef8611f562dbaa7fde1222f04d6fe4d5205
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2022-08-14 13:57:37 +0800

    Time: 22 ms (66.52%), Space: 19.8 MB (60.70%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int head = 0;
+        int tail = height.size() - 1;
+        
+        int max_left = height[head];
+        int max_right = height[tail];
+        
+        int area = 0;
+        
+        while(head < tail){
+            
+            if(max_left <= max_right){
+                ++head;
+                max_left = max(max_left, height[head]);
+                area += max_left - height[head];
+            }
+            else if(max_left > max_right){
+                --tail;
+                max_right = max(max_right, height[tail]);
+                area += max_right - height[tail];
+            }
+        }
+        
+        return area;
+    }
+};
