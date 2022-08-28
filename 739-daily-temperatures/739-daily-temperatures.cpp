adb67b994bcd4488d46479482baad4145179a6e2
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2022-08-28 13:08:55 +0800

    Time: 216 ms (75.35%), Space: 93 MB (6.62%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -7,27 +7,24 @@ public:
         int prev_day;
         int prev_temp;
         int n = temperatures.size();
-        stack<pair<int, int>> st;
         
+        stack<pair<int,int>> st;
         vector<int> res(n);
         
         for(int i = 0; i < n; i++){
             
             curr_day = i;
             curr_temp = temperatures[i];
-            
-            while(!st.empty() && st.top().second < curr_temp){
-                
+            while(!st.empty() && curr_temp > st.top().second){
                 prev_day = st.top().first;
                 prev_temp = st.top().second;
                 st.pop();
+                
                 res[prev_day] = curr_day - prev_day;
             }
             
             st.push({curr_day, curr_temp});
-            
         }
-        
         return res;
     }
 };
