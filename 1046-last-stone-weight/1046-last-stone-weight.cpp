7ad1559b6f96e57185de7c98e5afa8a41084d3f8
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2023-05-30 20:13:10 +0800

    Time: 0 ms (100.00%), Space: 7.7 MB (36.37%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-    
         priority_queue<int> pq;
-        
-        int x, y;
         
         for (auto s:stones) {
             pq.push(s);
         }
-
+        
         while (pq.size() > 1) {
-            x = pq.top();
+            int x = pq.top();
             pq.pop();
-            y = pq.top();
+            int y = pq.top();
             pq.pop();
-            if (x != y)
+            if (y != x) {
                 pq.push(abs(y - x));
+            }
         }
-        if (pq.empty())
+        
+        if (pq.empty()){
             return 0;
+        }
 
         return pq.top();
     }
