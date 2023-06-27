1fd5f3442116c1417de27811ba34d7dcf8a662ea
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2023-06-27 22:48:07 +0800

    Time: 43 ms (96.77%), Space: 16.5 MB (57.78%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -5,7 +5,6 @@ class Solution:
         
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
-                print(i+len(w))
                 if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
                     dp[i] = dp[i + len(w)]
                 if dp[i]:
