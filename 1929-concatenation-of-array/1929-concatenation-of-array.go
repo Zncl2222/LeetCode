8cf21c9f13a15025c02e8ad08352081a4789112e
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2023-09-27 23:08:24 +0800

    Time: 7 ms (71.82%), Space: 6.3 MB (25.43%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.go
+++ 1929-concatenation-of-array/1929-concatenation-of-array.go
@@ -1,10 +1,3 @@
 func getConcatenation(nums []int) []int {
-    n_length := len(nums)
-    res := make([]int, 2 * n_length)
-    for i := 0; i < 2; i++ {
-        for j := 0; j < n_length; j++ {
-            res[i * n_length + j] = nums[j]
-        }  
-    }
-    return res
+    return append(nums, nums...)
 }
