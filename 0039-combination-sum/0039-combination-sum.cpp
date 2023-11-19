a08f8c0398e51fbcf79583d51f37701a0bf067ab
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2023-11-20 00:36:17 +0900

    Time: 0 ms (100.00%), Space: 10.9 MB (97.82%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,34 +1,27 @@
 class Solution {
-    int _target;
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        _target = target;
-        vector<int> current;
-        vector<vector<int>> results;
-        
-        dfs(candidates, 0, 0, current, results);
-        return results;
+        std::vector<int> curr;
+        std::vector<std::vector<int>> res;
+        helper(candidates, target, 0, curr, res);
+        return res;
     }
 private:
-    void dfs(
-        vector<int>& candidates,
-        int sum,
-        int start,
-        vector<int>& current,
-        vector<vector<int>>& results
-    ) {
-        if (sum > _target) {
+    void helper(std::vector<int>& cands, int target, int i, std::vector<int>& curr,  std::vector<std::vector<int>>& res) {
+        if (i >= cands.size() || target < 0)
+            return;
+
+        if (target == 0) {
+            res.push_back(curr);
             return;
         }
-        if (sum == _target) {
-            results.push_back(current);
-            return;
-        }
+
+        curr.push_back(cands[i]);
         
-        for (int i = start; i < candidates.size(); i++) {
-            current.push_back(candidates[i]);
-            dfs(candidates, sum + candidates[i], i, current, results);
-            current.pop_back();
-        }
+        helper(cands, target - cands[i], i, curr, res);
+
+        curr.pop_back();
+
+        helper(cands, target, i + 1, curr, res);
     }
 };
