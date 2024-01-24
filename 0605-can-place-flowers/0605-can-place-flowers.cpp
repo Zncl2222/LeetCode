9983e32e00e87523f5c3235702194ba3f3365eb3
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2024-01-24 22:22:44 +0800

    Time: 11 ms (79.85%), Space: 22.7 MB (5.41%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -5,24 +5,13 @@ public:
             return true;
         }
         
+        flowerbed.insert(flowerbed.begin(), 0);
+        flowerbed.push_back(0);
+        
         int max_count = 0;
         int bed_size = flowerbed.size();
-        
-        if (bed_size == 1) {
-            if (n == 1 && flowerbed[0] == 0) {
-                return true;
-            }
-            return false;
-        }
-        
-        for (int i = 0; i < bed_size; i++) {
-            if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
-                flowerbed[i] = 1;
-                max_count++;
-            } else if (i != 0 && i != bed_size - 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
-                flowerbed[i] = 1;
-                max_count++;
-            } else if (i == bed_size - 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0) {
+        for (int i = 1; i < bed_size - 1; i++) {
+            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
                 flowerbed[i] = 1;
                 max_count++;
             }
