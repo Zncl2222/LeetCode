ce6c0651d37455c0149aab4085c4f0520d0094ef
Jian Yu, Chen <77830479+Zncl2222@users.noreply.github.com>
2024-01-24 22:21:00 +0800

    Time: 16 ms (39.88%), Space: 22.8 MB (5.41%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        if(n == 0){
+        if (n == 0) {
             return true;
         }
         
-        flowerbed.insert (flowerbed.begin(),0);
-        flowerbed.push_back(0);
-
-        for(int i = 1; i < flowerbed.size() - 1; i++){
-            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0){
-                flowerbed[i] = 1;
-                n--;
-            }
-            if (n == 0){
+        int max_count = 0;
+        int bed_size = flowerbed.size();
+        
+        if (bed_size == 1) {
+            if (n == 1 && flowerbed[0] == 0) {
                 return true;
             }
-
+            return false;
         }
-
-        return false;
+        
+        for (int i = 0; i < bed_size; i++) {
+            if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                max_count++;
+            } else if (i != 0 && i != bed_size - 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                max_count++;
+            } else if (i == bed_size - 1 && flowerbed[i - 1] == 0 && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                max_count++;
+            }
+        }
+        
+        if (max_count < n) {
+            return false;
+        }
+        
+        return true;
     }
 };
